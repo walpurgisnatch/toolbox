ceb04b18798328b1d3ab14b821e762a61ef9d95b
Walpurgisnatch <Kawemirs@gmail.com>
2021-07-30 04:00:24 -0400

    basic fileworks

--- src/fileworks.lisp
+++ src/fileworks.lisp
@@ -1,10 +1,30 @@
 (in-package :cl-user)
 (defpackage toolbox.fileworks
   (:use :cl)
-  (:export :mkdir))
+  (:export :mkdir
+   :ls
+           :create-with
+           :run-through))
 
 (in-package :toolbox.fileworks)
 
+
+(defun run-through (dir fn &key dirs (test (constantly t)))
+  (labels
+      ((run (name)
+         (cond
+           ((directory-pathname-p name)
+            (when (and dirs (funcall test name))
+              (funcall fn name))
+            (dolist (x (ls name)) (run x)))
+           ((funcall test name) (funcall fn name)))))
+    (run (pathname-as-directory dir))))
+
+(defun directory-wildcard (dir)
+  (make-pathname
+   :name :wild
+   :type :wild
+   :defaults (pathname-as-directory dir)))
 
 (defun component-present-p (value)
     (and value (not (eql value :unspecific))))
@@ -28,6 +48,15 @@
              :defaults pathname)
             pathname)))
 
+(defun ls (dir)
+  (when (wild-pathname-p dir)
+    (error "Wildcard not supported"))
+  (directory (directory-wildcard dir)))
+
 (defun mkdir (dir)
   (namestring (ensure-directories-exist
                (pathname-as-directory dir))))
+
+(defun create-with (file data)
+  (with-open-file (stream file :direction :output :if-exists :supersede :if-does-not-exist :create)
+    (write-sequence data stream)))
