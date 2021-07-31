6eb5820ba5bea6195c8e0d087e520510b95e0c38
walpurgisnatch <kawemirs@gmail.com>
2021-08-01 00:01:13 +1000

    truncate-name function

--- src/main.lisp
+++ src/main.lisp
@@ -1,14 +1,32 @@
 (in-package :cl-user)
 (defpackage toolbox
-  (:nicknames :omc)
+  (:nicknames :pls)
   (:use :cl :toolbox.utils)
   (:import-from :toolbox.fileworks
                 :mkdir
                 :ls
-   :run-through
-   :create-with)
+                :run-through
+                :create-with)
+  (:import-from :cl-ppcre
+                :regex-replace)
   (:export :ls
-           :create-with))
+   :create-with
+   :truncate-name))
 
 (in-package :toolbox)
+
+(defun shell (&rest args)
+  (uiop:run-program args :output t))
+
+(defun truncate-name (&rest args)
+  (destructuring-bind (dir sub)
+      (if (cdr args)
+          (list (car args)
+                (cadr args))
+          (list nil (car args)))
+    (loop for f in (ls dir)
+          with name do
+            (setf name (namestring f))
+          when (search sub name) do
+            (shell "mv" name (regex-replace sub name "")))))
 
