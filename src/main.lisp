3f032a19269f20dd3ef3a52dad1f65eea4d3dfa1
walpurgisnatch <kawemirs@gmail.com>
2021-08-01 13:37:28 +1000

    rename-regex, subst-in functions

--- src/main.lisp
+++ src/main.lisp
@@ -1,32 +1,47 @@
 (in-package :cl-user)
 (defpackage toolbox
-  (:nicknames :pls)
+  (:nicknames :wpb)
   (:use :cl :toolbox.utils)
   (:import-from :toolbox.fileworks
                 :mkdir
                 :ls
                 :run-through
                 :create-with)
+  (:import-from :toolbox.utils
+                :with-optional-dir)
   (:import-from :cl-ppcre
                 :regex-replace)
   (:export :ls
    :create-with
-   :truncate-name))
+           :truncate-name
+           :rename-regex
+           :subst-in))
 
 (in-package :toolbox)
 
 (defun shell (&rest args)
-  (uiop:run-program args :output t))
+  (format t "~&~{~a~^ ~}~%" args)
+  (uiop:run-program (format nil "~{~a~^ ~}" args) :output t))
 
 (defun truncate-name (&rest args)
-  (destructuring-bind (dir sub)
-      (if (cdr args)
-          (list (car args)
-                (cadr args))
-          (list nil (car args)))
+  (with-optional-dir (dir sub) args 
     (loop for f in (ls dir)
           with name do
             (setf name (namestring f))
           when (search sub name) do
             (shell "mv" name (regex-replace sub name "")))))
+
+(defun rename-regex (&rest args)
+  (with-optional-dir (dir old new) args 
+    (loop for f in (ls dir)
+          with name do
+            (setf name (namestring f))
+          when (search old name) do
+            (shell "mv" name (regex-replace old name new)))))
+
+
+(defun subst-in (old new &rest files)
+  (shell "sed" "-i" (format nil "'s/~a/~a/g' ~{~a~^ ~}" old new files)))
+
+
 
